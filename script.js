75d64fd9e477f90caa035bf220e5bc291cd8af8e
Sunil Peddamalli <sunilpeddamalli@gmail.com>
2020-12-20 20:24:42 -0800

    created navScrolling & operation Tabbed component

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
+/*Modal window*/
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove('hidden');
@@ -32,10 +32,7 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
-
+/*Scrolling*/
 btnScrollTo.addEventListener('click', function () {
   // const s1coords = section1.getBoundingClientRect();
   // window.scrollTo({
@@ -45,3 +42,34 @@ btnScrollTo.addEventListener('click', function () {
   // });
   section1.scrollIntoView({ behavior: 'smooth' });
 });
+
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// })
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href')
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+})
+
+/*Tabbed component*/
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content')
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  if (!clicked) return
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  clicked.classList.add('operations__tab--active');
+
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'))
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
+})
